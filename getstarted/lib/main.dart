9595569049737f12c75e16aaa97fb9a1d2a2406a
ItsJuniorDias <juniordias_@live.com>
2020-02-18 17:43:27 -0300

    Studing Components constructor

--- getstarted/lib/main.dart
+++ getstarted/lib/main.dart
@@ -6,18 +6,19 @@ void main() => runApp(new App());
 class App extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
-    
-    var texto = Text("Hello World!");
-
-    var centro = Center(child: texto,);
-
-    var appBar = AppBar(title: Text("App"));
- 
-    var scaffold = Scaffold(
-      appBar: appBar,
-      body: centro,
-      );
-
-    return MaterialApp(home: scaffold);
+     return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           backgroundColor: Colors.green,
+           title: Text("App")
+         ),
+        body: Center(
+          child: Text("Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World",
+           maxLines: 1,
+           overflow: TextOverflow.ellipsis,
+          )
+        ),
+       )
+     );
    }
 }
